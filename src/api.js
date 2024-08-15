9ade7a2416cdc4d190712c1bee2a2c9fd5fd06a2
NokoDev <elvisnoko18@gmail.com>
2024-08-15 14:51:35 +0200

    Added api.js

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoAPIOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'esGbwrm390msh2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+        'x-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
